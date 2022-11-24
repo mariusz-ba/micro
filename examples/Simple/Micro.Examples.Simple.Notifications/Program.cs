d608b702cd9a920a157258a5581a75ae28a2eb8f
Mariusz Baran <mariusz.baran23@gmail.com>
2022-11-24 22:56:17 +0100

    Use forwarded headers

--- examples/Simple/Micro.Examples.Simple.Notifications/Program.cs
+++ examples/Simple/Micro.Examples.Simple.Notifications/Program.cs
@@ -1,4 +1,19 @@
+using Micro.Common.ReverseProxy;
+using Micro.Common;
+using Micro.Contexts;
+using Micro.Observability.ApplicationInsights;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services
+    .AddMicro(builder.Configuration)
+    .AddContexts()
+    .AddObservability();
+    
 var app = builder.Build();
+
+app.UseReverseProxyHeaders();
+app.UseContexts();
+
 app.MapGet("/", () => "Micro.Examples.Simple.Notifications");
 app.Run();
