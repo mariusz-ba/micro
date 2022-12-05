3c035a2518b5dff9417a29f4f9c9c42649657816
Mariusz Baran <mariusz.baran23@gmail.com>
2022-12-05 11:25:22 +0100

    Remove unused serialization methods

--- src/Micro.Messaging.Abstractions/Serialization/IMessageSerializer.cs
+++ src/Micro.Messaging.Abstractions/Serialization/IMessageSerializer.cs
@@ -2,8 +2,6 @@ namespace Micro.Messaging.Abstractions.Serialization;
 
 public interface IMessageSerializer
 {
-    string Serialize<TMessage>(TMessage message);
-    TMessage? Deserialize<TMessage>(string message);
     byte[] SerializeBytes<TMessage>(TMessage message);
     TMessage? DeserializeBytes<TMessage>(byte[] message);
 }
--- src/Micro.Messaging.Abstractions/Serialization/JsonMessageSerializer.cs
+++ src/Micro.Messaging.Abstractions/Serialization/JsonMessageSerializer.cs
@@ -11,12 +11,6 @@ internal sealed class JsonMessageSerializer : IMessageSerializer
         Converters = { new JsonStringEnumConverter() }
     };
 
-    public string Serialize<TMessage>(TMessage message)
-        => JsonSerializer.Serialize(message, typeof(TMessage), JsonSerializerOptions);
-
-    public TMessage? Deserialize<TMessage>(string message)
-        => JsonSerializer.Deserialize<TMessage>(message, JsonSerializerOptions);
-
     public byte[] SerializeBytes<TMessage>(TMessage message)
         => JsonSerializer.SerializeToUtf8Bytes(message, JsonSerializerOptions);
 
